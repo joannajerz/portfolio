ea83f20700ec0ef7de2f3e61ed64c911191ec137
Joanna Jerz <joannajerz1@gmail.com>
2020-03-01 22:08:50 +0100

    feat: basic HTML

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,19 @@
-"use strict";
-const list=document.querySelector('.list--js')
-fetch('https://api.github.com/users/joannajerz/repos?sort=updated&direction=desc')
-.then(resp=>resp.json())
-.then(resp=>{
-  const repos=resp;
-  for(const repo of repos){
-    const {description, html_url, name} = repo;
-    list.innerHTML += `<li class = "list__item">
-    <a class="list__link" href="${html_url}">${name}</a>
-    <p>${description? description: "Brak opisu"}</p></li>`
-  }
-})
-.catch(err=>{
-  console.log(err)
-})
+// "use strict";
+// const list=document.querySelector('.list--js')
+// fetch('https://api.github.com/users/joannajerz/repos?sort=updated&direction=desc')
+// .then(resp=>resp.json())
+// .then(resp=>{
+//   const repos=resp;
+//   for(const repo of repos){
+//     const {description, html_url, name} = repo;
+//     list.innerHTML += `<li class = "list__item">
+//     <a class="list__link" href="${html_url}">${name}</a>
+//     <p>${description? description: "Brak opisu"}</p></li>`
+//   }
+// })
+// .catch(err=>{
+//   console.log(err)
+// })
 
-console.log(`Hello world!`);
 
 
